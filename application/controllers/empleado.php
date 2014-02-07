0cd7e3caffe5721bb90c287429c14e07122686d0
luismec90 <luismec90@gmail.com>
2014-02-07 11:25:20 -0500

    Consultar salarios

--- application/controllers/empleado.php
+++ application/controllers/empleado.php
@@ -295,7 +295,7 @@ class Empleado extends CI_Controller {
             $this->load->model('t_cargom');
             $data['lista_cargos'] = $this->t_cargom->listar_todas_los_cargos_por_depto($_GET['depto']);
         }
-        $filasPorPagina = 2;
+        $filasPorPagina = 20;
         if (empty($_GET["page"])) {
             $inicio = 0;
             $paginaActual = 1;
