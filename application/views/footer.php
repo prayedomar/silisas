378cbf9d63f2003dc423809678c1e783a62fda2b
Stevenson <Stevenson@Stevenson-PC>
2014-02-08 16:45:22 -0500

    Merge origin/master
    Conflicts:
    	application/controllers/empleado.php

--- application/views/footer.php
+++ application/views/footer.php
@@ -6,6 +6,23 @@
 <div id="coverDisplay">
     <img id="imgLoading" src="<?= base_url() ?>public/images/loading.gif">
 </div>
+<script>
+    (function(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function() {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-47882854-1', 'webfactional.com');
+    ga('send', 'pageview');
+
+</script>
 <?php if (isset($tab) && $tab == "consultar_sede") { ?>
     <script src='<?= base_url() ?>public/js/consultar_sede.js'></script>
 <?php } else if (isset($tab) && $tab == "consultar_salon") { ?>
@@ -28,6 +45,6 @@
     <script src='<?= base_url() ?>public/js/consultar_proveedor.js'></script>
 <?php } else if (isset($tab) && $tab == "consultar_caja") { ?>
     <script src='<?= base_url() ?>public/js/consultar_caja.js'></script>
-<?php } ?>
+<?php } ?>    
 </body>
 </html>
