80548fa8af6e52998f93ab9fcb896ab322566e8f
luismec90 <luismec90@gmail.com>
2014-02-12 17:53:37 -0500

    email v1

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -7,9 +7,9 @@ class Test extends CI_Controller {
     }
 
     function index() {
-        $this->sendEmail("asdas", "noreplay@sili.com", "prayedomar@hotmail.com", "Hola");
-        $this->sendEmail("asdas", "noreplay@sili.com", "luismec90@gmail.com", "Hola");
-        $this->sendEmail("asdas", "noreplay@sili.com", "silisascolombia@gmail.com", "Hola");
+        $this->sendEmail("asdas", "silisascolombia@gmail.com", "prayedomar@hotmail.com", "Hola");
+        $this->sendEmail("asdas", "silisascolombia@gmail.com", "luismec90@gmail.com", "Hola");
+        $this->sendEmail("asdas", "silisascolombia@gmail.com", "silisascolombia@gmail.com", "Hola");
     }
 
     function sendEmail($contenido, $from, $to, $asunto) {
@@ -18,7 +18,7 @@ class Test extends CI_Controller {
         $message = ob_get_clean();
         echo $message;
         $asunto = utf8_decode($asunto);
-        $headers = "From: SILI S.A.S \r\n";
+        $headers = "From: $from \r\n";
         $headers .= "MIME-Version: 1.0\r\n";
         $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
