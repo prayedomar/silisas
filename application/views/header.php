791d151e17351f06bfa16bbacd78bfb68878177f
Stevenson <Stevenson@Stevenson-PC>
2014-02-21 16:14:03 -0500

    Merge origin/master
    Conflicts:
    	application/views/footer.php

--- application/views/header.php
+++ application/views/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="es">
     <head>
-        <title>SILI - Administrador Sistema</title>
+        <title>SILI - Inicio</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
         <meta http-equiv="X-UA-Compatible" content="IE=9"/>
         <meta name="description" content="Inicio Directivo"/>
